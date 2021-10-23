99f70b5861f951fb543f22bf64efcdeb55d0174f
chelleaurelia <misyelaurelia@gmail.com>
2021-10-23 16:16:45 +0700

    category coredata + vc settings

---
+++ PokPok3/Response.swift
@@ -0,0 +1,15 @@
+//
+//  Response.swift
+//  PokPok3
+//
+//  Created by Michelle Aurelia on 23/10/21.
+//
+
+import Foundation
+
+struct ModelResponseDefault {
+  var query_status: Bool
+  var message: String
+  var data: Any?
+}
+
